46e790933aea0de81bd6f3061bb0ff71d39642b6
vs <vadim.sanzharov@graphics.cs.msu.ru>
2022-02-16 16:42:25 +0300

    edit

--- main.cpp
+++ main.cpp
@@ -14,11 +14,7 @@ int main(int argc, char* argv[])
 
   std::string gltf_path(argv[1]);
   std::string out_path(argv[2]);
-
-//  std::string gltf_path = "/home/vs/scenes/glTF-Sample-Models/2.0/Buggy/glTF/Buggy.gltf";
-//  std::string gltf_path = "/home/vs/scenes/glTF-Sample-Models/2.0/WaterBottle/glTF/WaterBottle.gltf";
-//  std::string hydra_out_path = "../hydra_out";
-
+  
   auto res = convert_gltf_to_hydra(gltf_path.c_str(), out_path.c_str());
 
   return !res;
